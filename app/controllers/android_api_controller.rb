096061f1fff94cdc2517abcae4f4f2cda3a2499b
Siddesh <siddesh.sh@styletag.com>
2013-11-15 00:40:48 +0530

    0002

--- app/controllers/android_api_controller.rb
+++ app/controllers/android_api_controller.rb
@@ -4,7 +4,7 @@ class AndroidApiController < ApplicationController
     resource = User.find_for_database_authentication(:email=>params[:email])
     if resource
       if resource.valid_password?(params[:password])
-        render :text => "true"
+        render :text => "true,#{resource.id}, #{resource.email}, #{resource.first_name}, #{resource.last_name}"
       else
         render :text => "false"
       end
