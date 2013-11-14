b9c5a99d2633f3ce9c0bf1ed3534d2619a6cdaf9
User <hemanth@user-Inspiron-5423.(none)>
2013-11-14 13:07:17 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ubiquity::Application.config.secret_token = 'efb7d379fdaf15bb4ccc0428ca17b62892a1ad3e0554689a27c666690c5a089801a5b19d4cc026ecafda1e95cae6c05986ce4dbf2d96334b63085c5b10d0d713'
