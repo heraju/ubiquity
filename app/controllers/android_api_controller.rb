f4778ad8fc8c988a30486c6ac30ec922e0e48661
User <hemanth@user-Inspiron-5423.(none)>
2013-11-15 00:46:07 +0530

    android_create_trip added

--- app/controllers/android_api_controller.rb
+++ app/controllers/android_api_controller.rb
@@ -3,4 +3,21 @@ class AndroidApiController < ApplicationController
   def android_connect_user
     
   end
+
+  def android_create_trip
+  	#http://www.cuputt.com/android_api/android_create_trip?userid=1&lat=12.96&long=77.56&type=1&number=500c
+  	@transport = Transport.new
+  	@transport.user_id = params['userid'].to_i
+  	@transport.transport_type_id = params['type'].to_i
+  	@transport.number = params['number']
+  	@transport.lat = params['lat']
+  	@transport.long = params['long']
+  	@transport.session = "active"
+  	if @transport.save
+        render :text => "true,#{@transport.id}"
+    else
+        render :text => "false"
+    end
+  end
+  	
 end
