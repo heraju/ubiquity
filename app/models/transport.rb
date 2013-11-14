8bab5d192257590388448cd9304e5dd8dac10112
Surya Tripathi <raj.surya19@gmail.com>
2013-11-15 04:04:50 +0530

    create point for shiphertz

--- app/models/transport.rb
+++ app/models/transport.rb
@@ -4,6 +4,33 @@ class Transport < ActiveRecord::Base
   has_many :statuses, as: :statusable
   belongs_to :transport_type
   belongs_to :user
+
+  after_create :create_geo_point
+  after_save :save_geo_point
+
+  def get_near_by_points(distanceInKM = '1')
+    geo = $geoService.get_near_by_points_by_max_distance($geoStorageName, self.lat, self.long, distanceInKM)
+    rescue Exception => e
+      e.message
+  end
+
+  def all_near_by_points(resultLimit = '10')
+    $geoService.get_near_by_point($geoStorageName, self.lat, self.long, resultLimit)
+    rescue Exception => e  
+      e.message
+  end
+
+  def create_geo_point
+    geoPointsList = Array.new()  
+    gp = App42::Geo::GeoPoint.new()  
+    gp.lat = self.lat
+    gp.lng = self.long
+    gp.marker = self.id
+    geoPointsList.push(gp)
+
+    geo = $geoService.create_geo_points($geoStorageName, geoPointsList) 
+  end
+
   def self.get_bus_information
     begin
       where(:session => "ACTIVE").map{|t| [t.lat,t.long,t.number]}
--- config/initializers/app42_ruby_api.rb
+++ config/initializers/app42_ruby_api.rb
@@ -1,9 +1,9 @@
-=begin
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/app42baas.yml")[Rails.env]
 
-api = App42::ServiceAPI.new(APP_CONFIG['api_key'],APP_CONFIG['secret_key'] )
+api = App42::ServiceAPI.new(APP_CONFIG['api_key'], APP_CONFIG['secret_key'] )
+
+$geoStorageName = "ubiquity_#{Rails.env.to_s}"
+
 $user_obj = api.buildUserService
 $social_service = api.buildSocialService
 $geoService = api.buildGeoService
- 
-=end
