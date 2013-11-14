bf6712060cade9b34edd14147b41aae4d93aebfa
Siddesh <siddesh.sh@styletag.com>
2013-11-15 00:34:16 +0530

    0002

--- app/controllers/android_api_controller.rb
+++ app/controllers/android_api_controller.rb
@@ -1,6 +1,16 @@
 class AndroidApiController < ApplicationController
   
   def android_connect_user
-    
+    resource = User.find_for_database_authentication(:email=>params[:email])
+    debugger
+    if resource
+      if resource.valid_password?(params[:password])
+        render :text => "true"
+      else
+        render :text => "false"
+      end
+    else
+      render :text => "false"
+    end
   end
 end
